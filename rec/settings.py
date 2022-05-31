672c8dd598efc1fce31986be0acd7bd63db68447
youssef <youssefouah1997@gmail.com>
2022-05-31 22:59:12 +0100

    postegres

--- rec/settings.py
+++ rec/settings.py
@@ -81,13 +81,27 @@ WSGI_APPLICATION = 'rec.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
         #'ENGINE': 'django.db.backends.postgresql',
        # 'NAME': 'postdb',
+    }
+    
+    
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

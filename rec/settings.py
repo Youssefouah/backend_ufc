66b3ca53a98c5f831d66e79dbe4e4da42990f0f6
Abdellatif <oussaid.abdellatif@gmail.com>
2022-06-05 03:30:06 +0100

    fix : fixed allowed hosts and postgres db

--- rec/settings.py
+++ rec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a+t-7^h3+jt^!dvs+1ukaz=l4a&1x6n#4&_*^o*j3)u944k)&f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost",'http://142.93.141.158:8080/','127.0.0.1','142.93.141.158']
+ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost",'http://138.68.170.41:8080/','138.68.170.41','127.0.0.1']
 
 
 
@@ -94,17 +94,26 @@ DATABASES = {
     
 }
 """
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'myproject',
+#         'USER': 'myprojectuser',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
+        'NAME': 'websudbackendnfc',
+        'USER': 'websuduser',
+        'PASSWORD': 'dell@123$Super',
         'HOST': 'localhost',
         'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
